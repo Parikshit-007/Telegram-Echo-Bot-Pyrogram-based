2bd7f2ed5e258d2f3bb8c1247e03ca2b35d47633
Parikshit07 <88080854+Parikshit-007@users.noreply.github.com>
2021-09-01 14:04:40 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,3 +1,3 @@
-api_id = ("Enter your api_id")
-api_hash = "("Enter your api_hash")"
-bot_token = "(Enter your bot_token")"
+api_id = (Enter your api_id)
+api_hash = ("Enter your api_hash")
+bot_token = ("Enter your bot_token")
